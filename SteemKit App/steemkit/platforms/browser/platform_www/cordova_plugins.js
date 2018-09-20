cdc4d62f05248a6e5be00a96cb80a7612b17607b
Genievot <jeevanjotsingh@yandex.com>
2018-09-20 18:04:45 +0530

    Mobile Qr code working
    Mobile qr code works
    More polished code added.

--- SteemKit App/steemkit/platforms/browser/platform_www/cordova_plugins.js
+++ SteemKit App/steemkit/platforms/browser/platform_www/cordova_plugins.js
@@ -1,20 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-qrscanner/www/www.min.js",
-        "id": "cordova-plugin-qrscanner.QRScanner",
-        "pluginId": "cordova-plugin-qrscanner",
-        "clobbers": [
-            "QRScanner"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-qrscanner/src/browser/plugin.min.js",
-        "id": "cordova-plugin-qrscanner.QRScannerProxy",
-        "pluginId": "cordova-plugin-qrscanner",
-        "runs": true
-    },
-    {
         "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
         "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
         "pluginId": "phonegap-plugin-barcodescanner",
@@ -33,7 +19,6 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-qrscanner": "2.6.0",
     "phonegap-plugin-barcodescanner": "8.0.0"
 }
 // BOTTOM OF METADATA
--- SteemKit App/steemkit/platforms/browser/www/js/app.js
+++ SteemKit App/steemkit/platforms/browser/www/js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   $('.tabs').tabs();
+  $('#scanqr').click(function(){
+    scanQr();
+  });
 
 });
 if(localStorage.getItem("LocalData") == null)
@@ -10,5 +13,30 @@ if(localStorage.getItem("LocalData") == null)
 }
 
 function scanQr(){
+  cordova.plugins.barcodeScanner.scan(
+          function (result) {
+              if(!result.cancelled)
+              {
+                  if(result.format == "QR_CODE")
+                  {
+                      navigator.notification.prompt("Please enter name of data",  function(input){
+                          var name = input.input1;
+                          var value = result.text;
 
+                          var data = localStorage.getItem("LocalData");
+                          console.log(data);
+                          data = JSON.parse(data);
+                          data[data.length] = [name, value];
+
+                          localStorage.setItem("LocalData", JSON.stringify(data));
+
+                          alert("Done");
+                      });
+                  }
+              }
+          },
+          function (error) {
+              alert("Scanning failed: " + error);
+          }
+     );
 }
